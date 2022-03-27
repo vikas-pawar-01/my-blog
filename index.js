e3699a59758bae7ef786d3f5cf71ad7e0cf0a4c3
Vikas Pawar <vicky.pawar17@gmail.com>
2022-03-27 23:27:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/users', (req, res, next) => {
+//     const data = fs.readFileSync(path.join(__dirname, 'data.json'), { encoding: 'utf8', flag: 'r' });
+//     res.send(data);
+//     return next();
+// });
 
 // app.get('/', (req, res, next) => {
 //     res.send('Welcome to Node!');
