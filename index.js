aacf571340a71557393cbe8b304fd04a2c314bd2
Vikas Pawar <vicky.pawar17@gmail.com>
2022-03-27 23:50:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
